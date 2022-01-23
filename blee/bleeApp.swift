ba0ca3fa9cb3e3b023976e48e3e5b7b7d0b7c5d0
jt <jt234wq@gmail.com>
2022-01-23 00:27:39 -0500

    Initial Commit

---
+++ blee/bleeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bleeApp.swift
+//  blee
+//
+//  Created by J Tan on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct bleeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
